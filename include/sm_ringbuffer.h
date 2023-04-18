5bb2c05b556d2e9ee32c147864addaeee7f01c88
fangcun010 <fangcun010@gmail.com>
2023-04-18 19:44:16 +0800

    Add base address support.

--- include/sm_ringbuffer.h
+++ include/sm_ringbuffer.h
@@ -1,22 +1,30 @@
 #ifndef SM_RINGBUFFER_SM_RINGBUFFER_H
 #define SM_RINGBUFFER_SM_RINGBUFFER_H
 
-typedef struct sm_ringbuffer_t {
+typedef struct sm_shared_ringbuffer_data_t {
     unsigned int in;
     unsigned int out;
     unsigned int mask;
     unsigned int element_size;
-    void *data;
+} sm_shared_ringbuffer_data_t;
+
+typedef struct sm_ringbuffer_t {
+    void *base_address;
+    sm_shared_ringbuffer_data_t *shared_ringbuffer_data;
 } sm_ringbuffer_t;
 
-extern unsigned int sm_ringbuffer_init(sm_ringbuffer_t *ringbuffer, void *buffer, unsigned int buffer_size,
+extern unsigned int sm_ringbuffer_init(sm_ringbuffer_t *ringbuffer, sm_shared_ringbuffer_data_t *shared_ringbuffer_data,
+                                       void *buffer, unsigned int buffer_size,
                                        unsigned int element_size);
 
-extern unsigned int sm_ringbuffer_peek_in_place(sm_ringbuffer_t *ringbuffer,void **data);
+extern unsigned int sm_ringbuffer_open(sm_ringbuffer_t *ringbuffer, sm_shared_ringbuffer_data_t *shared_ringbuffer_data,
+                                       void *base_address);
+
+extern unsigned int sm_ringbuffer_peek_in_place(sm_ringbuffer_t *ringbuffer, void **data);
 
 extern unsigned int sm_ringbuffer_skip_one(sm_ringbuffer_t *ringbuffer);
 
-extern unsigned int sm_ringbuffer_poke_in_place(sm_ringbuffer_t *ringbuffer,void **data);
+extern unsigned int sm_ringbuffer_poke_in_place(sm_ringbuffer_t *ringbuffer, void **data);
 
 extern unsigned int sm_ringbuffer_push_in_place(sm_ringbuffer_t *ringbuffer);
 
@@ -30,27 +38,38 @@ extern unsigned int sm_ringbuffer_full(sm_ringbuffer_t *ringbuffer);
 
 extern unsigned int sm_ringbuffer_length(sm_ringbuffer_t *ringbuffer);
 
-typedef struct sm_messagequeue_t {
+typedef struct sm_shared_messagequeue_data_t {
     unsigned int in;
     unsigned int out;
     unsigned int mask;
-    void *data;
+} sm_shared_messagequeue_data_t;
+
+typedef struct sm_messagequeue_t {
+    void *base_address;
+    sm_shared_messagequeue_data_t *shared_messagequeue_data;
 } sm_messagequeue_t;
 
-extern unsigned int sm_messagequeue_init(sm_messagequeue_t *messagequeue, void *buffer, unsigned int buffer_size);
+extern unsigned int sm_messagequeue_init(sm_messagequeue_t *messagequeue,
+                                         sm_shared_messagequeue_data_t *shared_messagequeue_data,
+                                         void *buffer, unsigned int buffer_size);
 
-extern unsigned int sm_messagequeue_peek_in_place(sm_messagequeue_t *messagequeue,void **data,
+extern unsigned int sm_messagequeue_open(sm_messagequeue_t *messagequeue,
+                                         sm_shared_messagequeue_data_t *shared_messagequeue_data,
+                                         void *base_address);
+
+extern unsigned int sm_messagequeue_peek_in_place(sm_messagequeue_t *messagequeue, void **data,
                                                   unsigned int *element_size);
 
 extern unsigned int sm_messagequeue_skip_one(sm_messagequeue_t *messagequeue);
 
-extern unsigned int sm_messagequeue_poke_in_place(sm_messagequeue_t *messagequeue,void **data,unsigned int element_size);
+extern unsigned int sm_messagequeue_poke_in_place(sm_messagequeue_t *messagequeue, void **data,
+                                                  unsigned int element_size);
 
 extern unsigned int sm_messagequeue_push_in_place(sm_messagequeue_t *messagequeue);
 
-extern unsigned int sm_messagequeue_push(sm_messagequeue_t *messagequeue, void *data,unsigned int element_size);
+extern unsigned int sm_messagequeue_push(sm_messagequeue_t *messagequeue, void *data, unsigned int element_size);
 
-extern unsigned int sm_messagequeue_pop(sm_messagequeue_t *messagequeue, void *data,unsigned int *element_size);
+extern unsigned int sm_messagequeue_pop(sm_messagequeue_t *messagequeue, void *data, unsigned int *element_size);
 
 extern unsigned int sm_messagequeue_empty(sm_messagequeue_t *messagequeue);
 
--- src/sm_ringbuffer.c
+++ src/sm_ringbuffer.c
@@ -14,14 +14,24 @@ unsigned int pow2rounddown(unsigned int x) {
     return 1 << (count - 1);
 }
 
-unsigned int sm_ringbuffer_init(sm_ringbuffer_t *ringbuffer, void *buffer, unsigned int buffer_size,
+unsigned int sm_ringbuffer_init(sm_ringbuffer_t *ringbuffer, sm_shared_ringbuffer_data_t *shared_ringbuffer_data,
+                                void *buffer, unsigned int buffer_size,
                                 unsigned int element_size) {
-    ringbuffer->in = 0;
-    ringbuffer->out = 0;
+    ringbuffer->shared_ringbuffer_data = shared_ringbuffer_data;
+    shared_ringbuffer_data->in = 0;
+    shared_ringbuffer_data->out = 0;
     unsigned int size = pow2rounddown(buffer_size / element_size);
-    ringbuffer->mask = size - 1;
-    ringbuffer->element_size = element_size;
-    ringbuffer->data = buffer;
+    shared_ringbuffer_data->mask = size - 1;
+    shared_ringbuffer_data->element_size = element_size;
+    ringbuffer->base_address = buffer;
+
+    return 1;
+}
+
+unsigned int sm_ringbuffer_open(sm_ringbuffer_t *ringbuffer, sm_shared_ringbuffer_data_t *shared_ringbuffer_data,
+                                void *base_address) {
+    ringbuffer->shared_ringbuffer_data = shared_ringbuffer_data;
+    ringbuffer->base_address = base_address;
 
     return 1;
 }
@@ -30,10 +40,13 @@ unsigned int sm_ringbuffer_push(sm_ringbuffer_t *ringbuffer, void *data) {
     if (sm_ringbuffer_full(ringbuffer))
         return 0;
 
-    void *dest_data = ringbuffer->data + (ringbuffer->in & ringbuffer->mask) * ringbuffer->element_size;
-    memcpy(dest_data, data, ringbuffer->element_size);
+    sm_shared_ringbuffer_data_t *shared_ringbuffer_data = ringbuffer->shared_ringbuffer_data;
+    void *dest_data = ringbuffer->base_address +
+                      (shared_ringbuffer_data->in & shared_ringbuffer_data->mask) *
+                      shared_ringbuffer_data->element_size;
+    memcpy(dest_data, data, shared_ringbuffer_data->element_size);
     atomic_signal_fence(memory_order_release);
-    ringbuffer->in++;
+    shared_ringbuffer_data->in++;
 
     return 1;
 }
@@ -42,7 +55,10 @@ unsigned int sm_ringbuffer_peek_in_place(sm_ringbuffer_t *ringbuffer, void **dat
     if (sm_ringbuffer_empty(ringbuffer))
         return 0;
 
-    void *src_data = ringbuffer->data + (ringbuffer->out & ringbuffer->mask) * ringbuffer->element_size;
+    sm_shared_ringbuffer_data_t *shared_ringbuffer_data = ringbuffer->shared_ringbuffer_data;
+    void *src_data = ringbuffer->base_address +
+                     (shared_ringbuffer_data->out & shared_ringbuffer_data->mask) *
+                     shared_ringbuffer_data->element_size;
     *data = src_data;
 
     return 1;
@@ -50,7 +66,7 @@ unsigned int sm_ringbuffer_peek_in_place(sm_ringbuffer_t *ringbuffer, void **dat
 
 unsigned int sm_ringbuffer_skip_one(sm_ringbuffer_t *ringbuffer) {
     atomic_signal_fence(memory_order_release);
-    ringbuffer->out++;
+    ringbuffer->shared_ringbuffer_data->out++;
 
     return 1;
 }
@@ -59,7 +75,9 @@ unsigned int sm_ringbuffer_poke_in_place(sm_ringbuffer_t *ringbuffer, void **dat
     if (sm_ringbuffer_full(ringbuffer))
         return 0;
 
-    void *dest_data = ringbuffer->data + (ringbuffer->in & ringbuffer->mask) * ringbuffer->element_size;
+    sm_shared_ringbuffer_data_t *shared_ringbuffer_data = ringbuffer->shared_ringbuffer_data;
+    void *dest_data = ringbuffer->base_address + (shared_ringbuffer_data->in & shared_ringbuffer_data->mask) *
+                                                 shared_ringbuffer_data->element_size;
     *data = dest_data;
 
     return 1;
@@ -67,7 +85,7 @@ unsigned int sm_ringbuffer_poke_in_place(sm_ringbuffer_t *ringbuffer, void **dat
 
 unsigned int sm_ringbuffer_push_in_place(sm_ringbuffer_t *ringbuffer) {
     atomic_signal_fence(memory_order_release);
-    ringbuffer->in++;
+    ringbuffer->shared_ringbuffer_data->in++;
 
     return 1;
 }
@@ -76,32 +94,46 @@ unsigned int sm_ringbuffer_pop(sm_ringbuffer_t *ringbuffer, void *data) {
     if (sm_ringbuffer_empty(ringbuffer))
         return 0;
 
-    void *src_data = ringbuffer->data + (ringbuffer->out & ringbuffer->mask) * ringbuffer->element_size;
-    memcpy(data, src_data, ringbuffer->element_size);
+    sm_shared_ringbuffer_data_t *shared_ringbuffer_data = ringbuffer->shared_ringbuffer_data;
+    void *src_data = ringbuffer->base_address + (shared_ringbuffer_data->out & shared_ringbuffer_data->mask) *
+                                                shared_ringbuffer_data->element_size;
+    memcpy(data, src_data, shared_ringbuffer_data->element_size);
     atomic_signal_fence(memory_order_release);
-    ringbuffer->out++;
+    ringbuffer->shared_ringbuffer_data->out++;
 
     return 1;
 }
 
 unsigned int sm_ringbuffer_empty(sm_ringbuffer_t *ringbuffer) {
-    return ringbuffer->in == ringbuffer->out;
+    return ringbuffer->shared_ringbuffer_data->in == ringbuffer->shared_ringbuffer_data->out;
 }
 
 unsigned int sm_ringbuffer_full(sm_ringbuffer_t *ringbuffer) {
-    return sm_ringbuffer_length(ringbuffer) == ringbuffer->mask + 1;
+    return sm_ringbuffer_length(ringbuffer) == ringbuffer->shared_ringbuffer_data->mask + 1;
 }
 
 unsigned int sm_ringbuffer_length(sm_ringbuffer_t *ringbuffer) {
-    return ringbuffer->in - ringbuffer->out;
+    return ringbuffer->shared_ringbuffer_data->in - ringbuffer->shared_ringbuffer_data->out;
 }
 
-unsigned int sm_messagequeue_init(sm_messagequeue_t *messagequeue, void *buffer, unsigned int buffer_size) {
-    messagequeue->in = 0;
-    messagequeue->out = 0;
+unsigned int sm_messagequeue_init(sm_messagequeue_t *messagequeue,
+                                  sm_shared_messagequeue_data_t *shared_messagequeue_data,
+                                  void *buffer, unsigned int buffer_size) {
+    messagequeue->shared_messagequeue_data = shared_messagequeue_data;
+    messagequeue->base_address = buffer;
+    shared_messagequeue_data->in = 0;
+    shared_messagequeue_data->out = 0;
     unsigned int size = pow2rounddown(buffer_size);
-    messagequeue->mask = size - 1;
-    messagequeue->data = buffer;
+    shared_messagequeue_data->mask = size - 1;
+
+    return 1;
+}
+
+unsigned int sm_messagequeue_open(sm_messagequeue_t *messagequeue,
+                                  sm_shared_messagequeue_data_t *shared_messagequeue_data,
+                                  void *base_address) {
+    messagequeue->shared_messagequeue_data = shared_messagequeue_data;
+    messagequeue->base_address = base_address;
 
     return 1;
 }
@@ -110,11 +142,12 @@ unsigned int sm_messagequeue_push(sm_messagequeue_t *messagequeue, void *data, u
     if (sm_messagequeue_full(messagequeue))
         return 0;
 
-    void *dest_data = messagequeue->data + (messagequeue->in & messagequeue->mask);
+    sm_shared_messagequeue_data_t *shared_messagequeue_data = messagequeue->shared_messagequeue_data;
+    void *dest_data = messagequeue->base_address + (shared_messagequeue_data->in & shared_messagequeue_data->mask);
     *((unsigned int *) dest_data) = element_size;
     memcpy(dest_data + sizeof(element_size), data, element_size);
     atomic_signal_fence(memory_order_release);
-    messagequeue->in += element_size + sizeof(element_size);
+    shared_messagequeue_data->in += element_size + sizeof(element_size);
 
     return 1;
 }
@@ -124,7 +157,8 @@ unsigned int sm_messagequeue_peek_in_place(sm_messagequeue_t *messagequeue, void
     if (sm_messagequeue_empty(messagequeue))
         return 0;
 
-    void *src_data = messagequeue->data + (messagequeue->out & messagequeue->mask);
+    sm_shared_messagequeue_data_t *shared_messagequeue_data = messagequeue->shared_messagequeue_data;
+    void *src_data = messagequeue->base_address + (shared_messagequeue_data->out & shared_messagequeue_data->mask);
     *element_size = *((unsigned int *) src_data);
     *data = src_data + sizeof(*element_size);
 
@@ -135,22 +169,24 @@ unsigned int sm_messagequeue_skip_one(sm_messagequeue_t *messagequeue) {
     if (sm_messagequeue_empty(messagequeue))
         return 0;
 
-    void *src_data = messagequeue->data + (messagequeue->out & messagequeue->mask);
+    sm_shared_messagequeue_data_t *shared_messagequeue_data = messagequeue->shared_messagequeue_data;
+    void *src_data = messagequeue->base_address + (shared_messagequeue_data->out & shared_messagequeue_data->mask);
     unsigned int element_size = *((unsigned int *) src_data);
 
     atomic_signal_fence(memory_order_release);
-    messagequeue->out += element_size + sizeof(element_size);
+    shared_messagequeue_data->out += element_size + sizeof(element_size);
 }
 
 unsigned int sm_messagequeue_pop(sm_messagequeue_t *messagequeue, void *data, unsigned int *element_size) {
     if (sm_messagequeue_empty(messagequeue))
         return 0;
 
-    void *src_data = messagequeue->data + (messagequeue->out & messagequeue->mask);
+    sm_shared_messagequeue_data_t *shared_messagequeue_data = messagequeue->shared_messagequeue_data;
+    void *src_data = messagequeue->base_address + (shared_messagequeue_data->out & shared_messagequeue_data->mask);
     *element_size = *(unsigned int *) src_data;
     memcpy(data, src_data + sizeof(*element_size), *element_size);
     atomic_signal_fence(memory_order_release);
-    messagequeue->out += *element_size + sizeof(*element_size);
+    shared_messagequeue_data->out += *element_size + sizeof(*element_size);
 
     return 1;
 }
@@ -159,7 +195,8 @@ unsigned int sm_messagequeue_poke_in_place(sm_messagequeue_t *messagequeue, void
     if (sm_messagequeue_full(messagequeue))
         return 0;
 
-    void *dest_data = messagequeue->data + (messagequeue->in & messagequeue->mask);
+    sm_shared_messagequeue_data_t *shared_messagequeue_data = messagequeue->shared_messagequeue_data;
+    void *dest_data = messagequeue->base_address + (shared_messagequeue_data->in & shared_messagequeue_data->mask);
     *((unsigned int *) dest_data) = element_size;
     *data = dest_data + sizeof(element_size);
 
@@ -170,22 +207,23 @@ unsigned int sm_messagequeue_push_in_place(sm_messagequeue_t *messagequeue) {
     if (sm_messagequeue_full(messagequeue))
         return 0;
 
-    void *dest_data = messagequeue->data + (messagequeue->in & messagequeue->mask);
+    sm_shared_messagequeue_data_t *shared_messagequeue_data = messagequeue->shared_messagequeue_data;
+    void *dest_data = messagequeue->base_address + (shared_messagequeue_data->in & shared_messagequeue_data->mask);
     unsigned int element_size = *((unsigned int *) dest_data);
     atomic_signal_fence(memory_order_release);
-    messagequeue->in += element_size + sizeof(element_size);
+    shared_messagequeue_data->in += element_size + sizeof(element_size);
 
     return 1;
 }
 
 unsigned int sm_messagequeue_empty(sm_messagequeue_t *messagequeue) {
-    return messagequeue->in == messagequeue->out;
+    return messagequeue->shared_messagequeue_data->in == messagequeue->shared_messagequeue_data->out;
 }
 
 unsigned int sm_messagequeue_full(sm_messagequeue_t *messagequeue) {
-    return sm_messagequeue_length(messagequeue) == messagequeue->mask + 1;
+    return sm_messagequeue_length(messagequeue) == messagequeue->shared_messagequeue_data->mask + 1;
 }
 
 unsigned int sm_messagequeue_length(sm_messagequeue_t *messagequeue) {
-    return messagequeue->in - messagequeue->out;
+    return messagequeue->shared_messagequeue_data->in - messagequeue->shared_messagequeue_data->out;
 }
